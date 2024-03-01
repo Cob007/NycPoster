bf90f309f3f78370c17bf6f942b68c9da2aca09e
Cob007 <godwill2beracah@outlook.com>
2024-03-01 12:38:21 -0500

    Mobile first responsive design on list added

--- component/custom/CustomState.js
+++ component/custom/CustomState.js
@@ -1,17 +1,18 @@
-const CustomState = (state) => {
-  return state === 1
+const CustomState = (state, message) => {
+
+  return state == 2
     ? `
     <section class="error-container"> 
         <img class="error-container__img" 
         src="./assets/icons/search.gif" alt="search gif" />
-        <p class='error-container__msg'>Ready to SEARCH</p>
+        <p class='error-container__msg'>Ready to  SEARCH</p>
     </section>
     `
     : `
     <section class="error-container"> 
         <img class="error-container__img" 
         src="./assets/icons/giphy.gif" alt="404 error gif" />
-        <p class='error-container__msg'>Ready to SEARCH</p>
+        <p class='error-container__msg'>${message}</p>
     </section>
     `;
 };
--- scripts/poster-api.js
+++ scripts/poster-api.js
@@ -1,34 +1,48 @@
 import CustomState from "../component/custom/CustomState.js";
 import Loading from "../component/loading/Loading.js";
-import ApiClient from "./api-client.js"
-import { API_KEY } from "./constant.js"
+import ApiClient from "./api-client.js";
+import { API_KEY } from "./constant.js";
+
+const appDivEl = document.querySelector(".app-container");
+
+
+const populateListEl = (posters, totalPages) => {
+
+}
 
 const getGetPosterBySearch = async (keyword) => {
-    const apiClient = new ApiClient(API_KEY, keyword);
-    populatePosterList(await apiClient.getMoviePoster());
-}
+  const apiClient = new ApiClient(API_KEY, keyword);
+  processApiRes(await apiClient.getMoviePoster());
+};
 
-const populatePosterList = (posters) => {
-    console.log("Poster : ", posters)
-}
+const processApiRes = (posters) => {
+  console.log("Poster search : ", posters.Search);
+  console.log("  : ", posters.totalResults);
+  console.log("Poster : ", posters.Error);
 
+  if (posters.Response === 'True') {
+    populateListEl(posters.Search, posters.totalResults )
+  } else if (posters.Response === 'False') {
+    console.log("Poster : ", posters.Response);
+    appDivEl.innerHTML = renderUi(1, posters.Error);
+  }
+};
 
+const formEl = document.querySelector(".header-container__search-form");
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  appDivEl.innerHTML = ""
+  const keyword = e.target.keyword.value;
+  getGetPosterBySearch(keyword);
+  e.target.keyword.value = "";
+});
 
-const formEl = document.querySelector('.header-container__search-form')
-formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const keyword =  e.target.keyword.value;
-    getGetPosterBySearch(keyword);
-    e.target.keyword.value = '' 
-})
+const renderUi = (state, msg) => {
+  return `
+        ${CustomState(state, msg)}
+    `;
+};
 
+//appDivEl.innerHTML  = renderUi(2, "")
 
-
-const appDivEl = document.querySelector('.app-container');
-const renderUi = () => {
-    return `
-        ${CustomState(1)}
-    `
-}
-//appDivEl.innerHTML = renderUi()
 
